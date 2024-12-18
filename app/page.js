119323772ac68d29f4b8e12f12fa37c8e03ab2e9
Robin Willis <robincwillis@gmail.com>
2024-12-18 17:57:03 -0600

    update copy

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
-import Image from "next/image";
+import Image from "next/image"
 
-import data from './data.json'; // Adjust path to your JSON file
+import data from './data.json'
 
 
 export default function Home() {
@@ -13,23 +13,28 @@ export default function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <h2>RCW</h2>
+        <h3>Architecture | Interior</h3>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        
+
         {projects.map((entry, index) => (
           <div key={index} style={{ width: '50%', position: 'relative' }}>
             <Image
-              src={entry.src} // Replace with the path to your image in the public directory
+              src={entry.src}
               alt="Description of the image"
               layout="responsive"
               width={100}
-              height={100} // These define the aspect ratio (adjust according to your image dimensions)
-              sizes="100vw" // Makes the image responsive to the viewport
-              priority // Optional: Preloads the image for better performance
+              height={100}
+              sizes="100vw"
+              priority
             />
-            <h2>{entry.alt}</h2>
+            <p>{entry.alt}</p>
           </div>
         ))}
       </main>
+      <footer>
+        <a href="#">robincwillis.com</a>
+      </footer>
     </div>
-  );
+  )
 }
